7d2e644e9e92157927a91ef614675f4d789896b4
Anatoliy Litinskiy <litinskiya@gmail.com>
2018-11-01 14:28:39 +0200

    bump modules version to use it with modern react, babel, webpack

--- webpack.config.js
+++ webpack.config.js
@@ -6,15 +6,19 @@ module.exports = {
     output: {
         path: path.resolve('lib'),
         filename: 'BoilerplateComponent.js',
-        libraryTarget: 'commonjs2'
+        libraryTarget: 'commonjs2',
     },
     module: {
         rules: [
             {
                 test: /\.jsx?$/,
                 exclude: /(node_modules)/,
-                use: 'babel-loader'
-            }
-        ]
-    }
-}
+                use: 'babel-loader',
+            },
+        ],
+    },
+    externals: {
+        // don't export React and other dependencies, use parent-project modules instead
+        'react': 'commonjs react',
+    },
+};
